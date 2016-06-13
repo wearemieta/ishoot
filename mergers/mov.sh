383afb36651f5fee92db319a794590889b8ce1f6
wearemieta <mi3ta@sent.as>
2016-06-13 18:51:05 +0200

    added: tests dir. refactored: mov.sh

--- mergers/mov.sh
+++ mergers/mov.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-# Download ffmpeg MacOS binary
-curl -O https://raw.githubusercontent.com/wearemieta/ishoot/master/bin/ffmpeg;
-
 # Setup working dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR;
 
+# Create local bin directory
+mkdir bin;
+
+# Download ffmpeg MacOS binary
+curl -o bin/ffmpeg https://raw.githubusercontent.com/wearemieta/ishoot/master/bin/ffmpeg;
+
+# Change permissions for ffmpeg
+chmod +x bin/ffmpeg
+
+
 # Create .ts files
 for file in *.mov;
-	do ffmpeg -i "$file" -c copy -bsf:v h264_mp4toannexb -f mpegts "${file%.mov}".ts;
+	do bin/ffmpeg -i "$file" -c copy -bsf:v h264_mp4toannexb -f mpegts "${file%.mov}".ts;
 done;
 
 # Merge .ts and render as mp4
-ffmpeg -f concat -i <(for f in *.ts; do echo "file '$(pwd)/$f'"; done) -c copy -bsf:a aac_adtstoasc output.mp4;
+bin/ffmpeg -f concat -safe 0 -i <(for f in *.ts; do echo "file '$(pwd)/$f'"; done) -c copy -bsf:a aac_adtstoasc output.mp4;
 
 # Remove unused .ts
 rm -rf *.ts;
