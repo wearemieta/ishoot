28d5dbe5e5caf3499b0f8fd2d22b2f6ae97efc14
wearemieta <mi3ta@sent.as>
2016-06-13 18:15:31 +0200

    added: mov merger

---
+++ mergers/mov.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Download ffmpeg MacOS binary
+
+# Setup working dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR;
+
+# Create .ts files
+for file in *.mov;
+	do ffmpeg -i "$file" -c copy -bsf:v h264_mp4toannexb -f mpegts "${file%.mov}".ts;
+done;
+
+# Merge .ts and render as mp4
+ffmpeg -f concat -i <(for f in *.ts; do echo "file '$(pwd)/$f'"; done) -c copy -bsf:a aac_adtstoasc output.mp4;
+
+# Remove unused .ts
+rm -rf *.ts;
+
+# Exit
+echo "All done";
+exit 0
